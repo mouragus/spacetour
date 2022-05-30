897b414eb2e0034683ff6a7fc092640f9291e7b4
BrenoMMassa <brenommassa@gmail.com>
2022-05-30 14:00:59 -0300

    pequenos ajustes JS.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const botaoSubmit = document.getElementById('btnEnviar')
 
 botaoSubmit.addEventListener("click", ()=>{
-    const campos = document.querySelectorAll("input[type='text'],input[type='text'],input[type='text'],input[type='radio'],input[type='radio'],input[type='date'],input[type='text'],input[type='button']")
+    const campos = document.querySelectorAll("input[type='text'],input[type='email'],input[type='radio'],input[type='date']")
 
     for(let x = 0 ; x < campos.length ; x++){
         if(campos[x].value == ""){
